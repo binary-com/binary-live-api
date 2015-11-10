85f907de77a51b9c4e7426bd3d30c004adc28789
Boris Yankov <borisyankov@gmail.com>
2015-11-10 15:32:27 +0200

    Fix merge issue, rebuild for file case fix

--- src/LiveApi.js
+++ src/LiveApi.js
@@ -72,14 +72,14 @@ export default class LiveApi {
 
     onError(error) {
         // for process manager like pm2.
-+        // It's necessary to print error with console.error.
-+        // It will make error readable on error.log
-+        console.error(error);
-+        
-+        // And also make process exiting to respawn.
-+        if (typeof process !== "undefined") {
-+            process.exit();
-+        }
+        // It's necessary to print error with console.error.
+        // It will make error readable on error.log
+        console.error(error);
+
+        // And also make process exiting to respawn.
+        if (typeof process !== "undefined") {
+            process.exit();
+        }
     }
 
     onMessage(message) {
