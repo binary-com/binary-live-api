0726263352ed4e08af8c500c4a689e5c724277ad
qingwei <qingwei@binary.com>
2016-02-02 17:19:02 +0800

    Fix subscriptions updates

--- src/LiveApi.js
+++ src/LiveApi.js
@@ -169,7 +169,7 @@ export default class LiveApi {
     resubscribe() {
         const { ticks, priceProposal } = this.subscriptions;
 
-        this.subscribeToTicks(ticks);
+        this.subscribeToTicks(Object.keys(ticks));
 
         if (priceProposal) {
             this.subscribeToPriceForContractProposal(priceProposal);
@@ -296,7 +296,7 @@ export default class LiveApi {
     }
 
     subscribeToTicks(symbols) {
-        this.subscriptions.ticks = symbols;
+        symbols.forEach(s => this.subscriptions.ticks[s] = true);
 
         this.send({
             ticks: symbols,
