c57c94683bdd50b5a3e17ac61a851816b334577f
Qing Wei <qingwei@binary.com>
2016-08-30 16:43:36 +0800

    Add test for resubscribe (#71)

--- src/__tests__/resubscribe-test.js
+++ src/__tests__/resubscribe-test.js
@@ -1,22 +1,53 @@
 import chai, { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import spies from 'chai-spies';
+chai.use(spies);
 chai.use(chaiAsPromised);
 
 import LiveApi from '../LiveApi';
-import ws from 'ws';
+import * as stateful from '../stateful';
+import WS from 'ws';
 
 
 describe('resubscribe', () => {
-    let liveApi;
-
+    let connection;
+    let api;
     beforeEach(() => {
-        liveApi = new LiveApi({ websocket: ws });
+        connection = new WS('wss://ws.binaryws.com/websockets/v3');
+        api = new LiveApi({ connection, websocket: WS });
     });
 
-	it('todo', () => {
-		// disconnect, check if resubscribe is called
-        expect(true).to.equal(true);
-	});
+    afterEach(() => {
+        api.disconnect();
+    });
+
+    it('should reconnect when disconnected', done => {
+
+        connection.close();
+        setTimeout(() => {
+            expect(api.ping()).to.eventually.have.property('ping');
+            done();
+        }, 3000);
+    });
+
+    it('should resubscribe all subscription after reconnect', done => {
+
+        const api = new LiveApi({ connection });
+
+        const spy = chai.spy();
+        api.events.on('tick', spy);
+
+        const ticks = ['R_100'];
+        api.subscribeToTicks(ticks);
+
+        connection.close();
+
+        setTimeout(() => {
+            expect(stateful.getState().ticks.has('R_100')).to.equal(true);
+            expect(spy).to.have.been.called();
+            done();
+        }, 5000);
+    });
 
     // check if empty state, and no resubsription when new
     // check for specific resubsriptions
