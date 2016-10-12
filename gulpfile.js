bf3dd5b475663c70c5a4b315979248f82b2634d7
qingwei <qingwei@binary.com>
2016-10-12 12:13:39 +0800

    Fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,27 @@
 var gulp        = require('gulp');
-var deploy      = require('gulp-gh-pages');
+var gh          = require('gulp-gh-pages');
 var webpack     = require('webpack');
 var gutil       = require('gulp-util');
+var pkg         = require('./package.json');
 /**
  * Push build to gh-pages
  */
 
 gulp.task('build', function(callback) {
-    webpack({}, function(err, stats) {
+    webpack(require('./webpack.config.js'), function(err, stats) {
         if(err) throw new gutil.PluginError("webpack", err);
         gutil.log("[webpack]", stats.toString());
+
+        var v = pkg.version;
+
+        gulp.src(['lib/*.*'])
+            .pipe(gulp.dest('lib/' + v));
+
         callback();
     });
 });
 
 gulp.task('deploy', ['build'], function () {
     return gulp.src("./lib/**/*")
-        .pipe(deploy());
+        .pipe(gh({ force: true }));
 });
