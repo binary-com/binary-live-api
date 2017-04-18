5eef827c2df7d0fc6b4a8bc4a4962cf8d9a19411
Amin Marashi <marashi.s.amin@gmail.com>
2017-04-18 10:38:01 +0800

    4 spaces indentation for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,33 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  devtool: 'source-map',
-  entry: ['./src/index.js'],
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: path.join(__dirname, 'src'),
-      },
-      {
-        test: /\.js$/,
-        loader: 'eslint-loader',
-        include: path.join(__dirname, 'src'),
-      },
+    devtool: 'source-map',
+    entry: ['./src/index.js'],
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+            },
+        }),
     ],
-  },
-  output: {
-    library: 'binary-live-api',
-    libraryTarget: 'umd',
-    path: path.resolve(__dirname, 'lib'),
-    filename: 'binary-live-api.js',
-  },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: path.join(__dirname, 'src'),
+            },
+            {
+                test: /\.js$/,
+                loader: 'eslint-loader',
+                include: path.join(__dirname, 'src'),
+            },
+        ],
+    },
+    output: {
+        library: 'binary-live-api',
+        libraryTarget: 'umd',
+        path: path.resolve(__dirname, 'lib'),
+        filename: 'binary-live-api.js',
+    },
 };
